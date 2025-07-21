2b0748afc1e7cb996bd957eea1a0f677153aa441
anikinthos <hayatata@live.jp>
2025-07-21 13:46:19 +0900

    add: /userのGETリクエストを実装

--- main.go
+++ main.go
@@ -4,7 +4,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"encoding/json"
+	"os"
 )
+
+var debugMode = os.Getenv("DEBUG") == "true"
+
+// Debugログ出力用の関数
+func debugLog(format string, value ...interface{}) { //interface{}は任意の型
+	if debugMode {
+		log.Printf("[DEBUG] " + format, value... )
+	}
+}
+
+// ユーザー定義
+type User struct {
+	Id int `json:"id"` //構造体タグあり
+	Name string `json:"name"`
+}
+
+var users = []User{
+	{ Id: 1, Name: "Alice" },
+	{ Id: 2, Name: "Bob" },
+}
 
 func handler ( w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Welcome Go Playground!!")
@@ -18,12 +40,31 @@ func handler_hello_world ( w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello, World!!!!")
 }
 
+func handler_users ( w http.ResponseWriter, r *http.Request) {
+	// レスポンスヘッダーを設定
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+
+	// ユーザー情報をJSON形式で返す
+	jsonBlock, _ := json.Marshal(users)
+	debugLog("JSON Response: %s", string(jsonBlock))
+
+	// エラーハンドリング
+
+	// レスポンスにJSONデータを書き込む
+	fmt.Fprint(w, string(jsonBlock))
+}
+
 func main() {
 	var httpServer http.Server
+	
+	// ルーティング設定
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/hello_world", handler_hello_world)
+	http.HandleFunc("/users", handler_users)
+
+
+	// サーバーの設定
 	log.Println("start http listening :18888")
 	httpServer.Addr = ":18888"
 	log.Println(httpServer.ListenAndServe())
-
 }
